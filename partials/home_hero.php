4557259400f73b12869693348edd9c24e46e5ff4
Dale Sande <dale.sande@gmail.com>
2011-05-24 00:44:07 -0500

    added twitter logo to home page

--- partials/home_hero.php
+++ partials/home_hero.php
@@ -1,5 +1,6 @@
 <article class="hero linkbox">	
-	<div class="no-ie"><a href="/no-ie.php" title="IE not currently supported">What is Axle's browser support?</a></div>		
+	<div class="no-ie"><a href="/no-ie.php" title="IE not currently supported">What is Axle's browser support?</a></div>	
+	<div class="twitter-ico"><a href="http://twitter.com/#!/anotheruiguy" title="follow me @anotheruiguy">follow me @anotheruiguy</a></div>	
 	<hgroup>
 		<h1>Axle.LESS</h1>
 		<h4>Putting 'Object' in OOCSS since 2011</h4>
